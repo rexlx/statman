a25f6c5f65435262a3589324f600e4ae8e0c1140
rexlx <rxlx@nullferatu.com>
2024-02-20 13:01:09 -0600

    add comments

--- example/client.py
+++ example/client.py
@@ -5,24 +5,40 @@ from datetime import datetime as dt
 import time
 
 def get_data(i, url):
+    """perform a GET request to the given URL and return the response as a dictionary
+    This is only to demonstrate "doing something" so we can send some stats to the statwriter
+    """
+
+    # the response dictionary
     res = {
         "value": [],
         "time": None,
         "id": None,
         "extra": []
     }
+
+    # perform the GET request
     response = requests.get(url)
+
+    # try to parse the response and fill the dictionary
     try:
+        # this is the time in ISO 8601 format + Z
         t = dt.now().strftime("%Y-%m-%dT%H:%M:%S")
         t += "Z"
+        
         data = response.json()
+
+        # i happen to know in this case that the response is a dictionary with a key 'data'
         out = data['data']
+
         elapsed = response.elapsed.total_seconds()
+
         res['value'] = [elapsed]
         res['time'] = t
         res['id'] = out
         res['extra'] = [{"iteration": i, "url": url}]
     except Exception as e:
+        # dont blow up if something goes wrong, just inform the user
         print(e)
     return res
 
@@ -33,7 +49,7 @@ def post_to_statwriter(data, url):
 def main():
     resource_uri = "https://namer.nullferatu.com"
     statwriter_uri = "http://cobra.nullferatu.com:20080/namer-stats"
-    for i in range(0, 1000):
+    for i in range(0, 10):
         data = get_data(i, resource_uri)
         post_to_statwriter(data, statwriter_uri)
         time.sleep(2)
